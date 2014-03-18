3d686e237c3b53ffe15a9ba39714b52f7ea9dcf1
Paul Blouët <paul@paulblouet.fr>
2014-03-18 18:18:47 +0100

    some more goals

--- Main.hs
+++ Main.hs
@@ -1,15 +1,18 @@
 module Main where
 
+import Control.Monad
+
 import System.Console.Terminfo.Base
 import System.Console.Terminfo.Effects
 import System.Console.Terminfo.Color
 
 boldText :: Capability (TermOutput)
 boldText = do
-  bold  <- withBold
-  color <- withForegroundColor
+  bold      <- withBold
+  color     <- withForegroundColor
+  boldcolor <- mplus withBold withForegroundColor
   return (termText "normal text " <#> bold (termText "bold text") <#> termText " normal text\n" <#>
-          (bold . (color Blue)) (termText "bold and blue") <#> termText " normal again")
+          boldcolor Blue (termText "bold and blue") <#> termText " normal again")
 
 main :: IO ()
 main = do
