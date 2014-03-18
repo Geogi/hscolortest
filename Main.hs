cebab57b14b9308937b40f5043cff5d758a7410a
Paul Blouët <paul@paulblouet.fr>
2014-03-18 17:39:32 +0100

    got something working but not pretty

--- Main.hs
+++ Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import System.Console.Terminfo.Base
+import System.Console.Terminfo.Effects
+
+boldText :: Capability (TermOutput)
+boldText = do
+  out  <- tiGetOutput1 "bold"
+  bold <- boldOn :: Capability (TermOutput)
+  return (out <#> bold <#> termText "bold text")
+
 main :: IO ()
-main = putStrLn "test"
+main = do
+  term <- setupTermFromEnv
+  case getCapability term boldText of
+    Just to -> runTermOutput term to
+    _       -> putStrLn "E: no term support"
+  
