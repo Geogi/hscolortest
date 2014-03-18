404156f0baf722e0f32ced9194f7e6036e7026c4
Paul Blouët <paul@paulblouet.fr>
2014-03-18 17:50:58 +0100

    duplicates instead of mixing

--- Main.hs
+++ Main.hs
@@ -2,11 +2,14 @@ module Main where
 
 import System.Console.Terminfo.Base
 import System.Console.Terminfo.Effects
+import System.Console.Terminfo.Color
 
 boldText :: Capability (TermOutput)
 boldText = do
-  bold <- withBold
-  return (termText "normal text " <#> bold (termText "bold text") <#> termText " normal text\n")
+  bold  <- withBold
+  color <- withForegroundColor
+  return (termText "normal text " <#> bold (termText "bold text") <#> termText " normal text\n" <#>
+          (bold <#> color Blue) (termText "bold and blue") <#> termText " normal again")
 
 main :: IO ()
 main = do
