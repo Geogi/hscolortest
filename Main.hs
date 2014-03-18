0c7dce27878321dc3b63bafa5f0729a9fb16d90e
Paul Blouët <paul@paulblouet.fr>
2014-03-18 21:13:56 +0100

    still working

--- Main.hs
+++ Main.hs
@@ -10,6 +10,10 @@ import Format
 someText :: Formatted
 someText = format "some text\n" Blue
 
+moreText :: Formatted
+moreText = formats "more text\n" [Red]
+
 main :: IO ()
 main = do
   fprint someText
+  fprint moreText
