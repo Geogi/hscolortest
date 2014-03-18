25ec8caaa8f1bb3705fa790f4ab8d64ea03361fb
Paul Blouët <paul@paulblouet.fr>
2014-03-18 18:48:57 +0100

    bold red ok

--- Main.hs
+++ Main.hs
@@ -6,13 +6,24 @@ import System.Console.Terminfo.Base
 import System.Console.Terminfo.Effects
 import System.Console.Terminfo.Color
 
+withBoldRed :: TermStr s => Capability (s -> s)
+withBoldRed = do
+  bold  <- boldOn
+  color <- withForegroundColor
+  stop  <- allAttributesOff
+  return (\str -> bold <#> color Red str <#> stop)
+
 boldText :: Capability (TermOutput)
 boldText = do
-  bold      <- withBold
-  color     <- withForegroundColor
-  boldcolor <- mplus withBold withForegroundColor
-  return (termText "normal text " <#> bold (termText "bold text") <#> termText " normal text\n" <#>
-          boldcolor Blue (termText "bold and blue") <#> termText " normal again")
+  bold  <- withBold
+  color <- withForegroundColor
+  under <- withUnderline
+  boldred <- withBoldRed
+  standout <- withStandout
+  return (termText "normal text " <#> bold (termText "bold text") <#>
+          termText " normal text\n" <#> under (termText "underlined") <#>
+          color Blue (termText " blue\n") <#>
+          standout (termText "standout") <#> boldred (termText " bb\n"))
 
 main :: IO ()
 main = do
